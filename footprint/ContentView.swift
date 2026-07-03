89ed3c59496ad2d926ad18afe4c1ed0280c55907
wuyou012 <tangdi0110@gmail.com>
2026-07-03 17:48:44 +0800

    Initial Commit

---
+++ footprint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  footprint
+//
+//  Created by wuyou on 3/7/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ footprint/footprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  footprintApp.swift
+//  footprint
+//
+//  Created by wuyou on 3/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct footprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
